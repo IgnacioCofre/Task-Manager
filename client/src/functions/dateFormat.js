747ddb0ffcdcfa262a42710ed77485abdd34b8a7
Ignacio Cofré <ignacio.cofre.14@sansano.usm.cl>
2022-03-26 11:49:45 -0300

    arreglos a los estados de las tareas

--- client/src/functions/dateFormat.js
+++ client/src/functions/dateFormat.js
@@ -1,6 +1,6 @@
 export default function getDateFormat (date) {
     const day = date.getDate() > 9 ? `${date.getDate()}` : `0${date.getDate()}`;
-    const month = date.getMonth() > 9 ? `${date.getMonth()}` : `0${date.getMonth()}`;
+    const month = date.getMonth() > 9 ? `${date.getMonth()+1}` : `0${date.getMonth()+1}`;
     return `${day}/${month === '00' ? '01': month}/${date.getFullYear()}`
 }
 
--- client/src/functions/orderTasks.js
+++ client/src/functions/orderTasks.js
@@ -1,7 +1,8 @@
 function orderTasksBy (orderBy) {
     return function(Task1, Task2) {  
-        //Order by creationDate or expirationDate
-        if ( orderBy === "creationDate" || orderBy === "expirationDate" ) {
+        
+        //Order by creationDate or expirationDate    
+        if (orderBy === "expirationDate") {
             if (new Date(Task1[orderBy]) < new Date(Task2[orderBy]) ) {  
                 return -1;  
             } else {  
@@ -10,17 +11,40 @@ function orderTasksBy (orderBy) {
         //Order by task state
         } else if (orderBy === "currentState") {
             const today = new Date();
-            const stateTask1 = today - new Date(Task1["expirationDate"]);
-            const stateTask2 = today - new Date(Task2["expirationDate"]);
+
+            const state = {
+                "expire" : 3,
+                "toDo": 2,
+                "completed": 1,
+            }
+            
+            var stateTask1 = state["toDo"], stateTask2 = state["toDo"];
+
+            if (Task1["completed"]) {
+                stateTask1 = state["completed"];
+            } else if ((new Date(Task1["expirationDate"]) - today) >= 0) {
+                stateTask1 = state["toDo"];
+            } else {
+                stateTask1 = state["expire"]
+            }
+
+            if (Task2["completed"]) {
+                stateTask2 = state["completed"];
+            } else if ((new Date(Task2["expirationDate"]) - today) >= 0) {
+                stateTask2 = state["toDo"];
+            } else {
+                stateTask2 = state["expire"]
+            }
     
             if (stateTask1 > stateTask2) {  
                 return -1;  
             } else {  
                 return 1;  
             } 
+
         //Default order by "creationDate"
         } else {
-            if (new Date(Task1["creationDate"]) < new Date(Task2["creationDate"]) ) {  
+            if (new Date(Task1["creationDate"]) > new Date(Task2["creationDate"]) ) {  
                 return -1;  
             } else {  
                 return 1;  
